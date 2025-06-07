cab5015e077b80d19eaf776bdc401883c7f46ce9
shtefko55 <st.todorov@proton.me>
2025-06-07 10:34:16 +0300

    Updated package.json

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -142,12 +142,12 @@ CommandShortcut.displayName = "CommandShortcut"
 
 export {
   Command,
-  CommandDialog,
+  
   CommandInput,
   CommandList,
   CommandEmpty,
   CommandGroup,
   CommandItem,
-  CommandShortcut,
-  CommandSeparator,
+  
+  
 }
